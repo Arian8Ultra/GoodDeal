31976705e241858e1ed006ff0acc543c12a9e88b
MPORG\17027 <17027@mporg.ir>
2023-06-19 12:46:20 +0330

    new map and leaflet

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,30 +1,107 @@
-import React from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import { MenuBook, MenuBookRounded, MenuRounded } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
-import { Stack } from "@mui/system";
-import { useNavigate } from "react-router-dom";
-import IButton from "../../components/IButton";
+import {
+  MapContainer,
+  TileLayer
+} from "react-leaflet";
+import { useLocation, useParams } from "react-router-dom";
+import "./Shop.css";
 
 const Shop = () => {
-    let {shopId, categoryId} = useParams()
-    let shop = useLocation().state
+  let { shopId, categoryId } = useParams();
+  let shop = useLocation().state.shop;
   return (
     <Box
-    display={'grid'}
-    gridTemplateColumns={{
-      xs: 'repeat(1, 1fr)',
-      sm: 'repeat(1, 1fr)',
-      md: 'repeat(2, 1fr)',
-      lg: 'repeat(2, 1fr)',
-      xl: 'repeat(2, 1fr)',
-    }}
-    gap={'1rem'}
-    padding={'1rem'}
+      display={"grid"}
+      gridTemplateColumns={{
+        xs: "repeat(1, 1fr)",
+        sm: "repeat(1, 1fr)",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(2, 1fr)",
+        xl: "repeat(2, 1fr)",
+      }}
+      gap={"1rem"}
+      padding={"1rem"}
     >
+      <Box
+        display={"grid"}
+        gridTemplateColumns={{
+          xs: "repeat(1, 1fr)",
+          sm: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+        }}
+        gap={"1rem"}
+        padding={"1rem"}
+      >
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Typography variant={"h6"} component={"h6"}>
+            نام فروشگاه :
+          </Typography>
+          <Typography variant={"h6"} component={"h6"}>
+            {shop?.name}
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Typography variant={"h6"} component={"h6"}>
+            نام صاحب فروشگاه :
+          </Typography>
+          <Typography variant={"h6"} component={"h6"}>
+            {shop?.ownerFullName}
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gridColumn={"1/3"}
+        >
+          <Typography variant={"h6"} component={"h6"}>
+            آدرس :
+          </Typography>
+          <Typography variant={"h6"} component={"h6"}>
+            {shop?.address}
+          </Typography>
+        </Box>
+      </Box>
 
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"100%"}
+      >
+        <MapContainer
+          // @ts-ignore
+          center={[35.689720986449565, 51.47891772707084]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{
+            border: " 2px solid #4763E4",
+            width: "80%",
+            height: "437px",
+            borderRadius: "15px",
+            zIndex: 0,
+            backgroundColor: "rgba(255,255,255,0.5)",
+            backdropFilter: "blur(10px)",
+          }}
+        >
+          <TileLayer
+            // @ts-ignore
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
